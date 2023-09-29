50078491a65231b169894f2504f9d7bdb3c00157
classworm <classworm@icloud.com>
2023-09-29 11:50:33 +0300

    Item and Inventory

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Item : MonoBehaviour
+{
+    private bool _wasPickedUp;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_wasPickedUp)
+        {
+            return;
+        }
+
+        var inventory = other.GetComponent<Inventory>();
+        if (inventory != null)
+        {
+            inventory.Pickup(this);
+            _wasPickedUp = true;
+        }
+    }
+
+    private void OnValidate()
+    {
+        var collider = GetComponent<Collider>();
+        collider.isTrigger = true;
+    }
+}
