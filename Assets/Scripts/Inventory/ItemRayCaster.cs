1c38c38e56df1ee2480445b834a3a410a409c6a3
classworm <classworm@icloud.com>
2023-10-10 23:52:14 +0300

    ItemRayCaster

---
+++ Assets/Scripts/Inventory/ItemRayCaster.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class ItemRayCaster : ItemComponent
+{
+    [SerializeField] private float _delay = 0.1f;
+    [SerializeField] private float _range = 10f;
+    
+    private RaycastHit[] _results = new RaycastHit[10];
+    private int _layermask;
+
+    private void Awake()
+    {
+        _layermask = LayerMask.GetMask("Default");
+    }
+
+    public override void Use()
+    {
+        _nextUseTime = Time.time + _delay;
+
+        Ray ray = Camera.main.ViewportPointToRay(Vector3.one / 2f);
+        int hits = Physics.RaycastNonAlloc(ray, _results, _range, _layermask, QueryTriggerInteraction.Collide);
+
+        RaycastHit nearest = new RaycastHit();
+        double nearestDistance = double.MaxValue;
+        
+        for (int i = 0; i < hits; i++)
+        {
+            var distance = Vector3.Distance(transform.position, _results[i].point);
+            if (distance < nearestDistance)
+            {
+                nearest = _results[i];
+                nearestDistance = distance;
+            }
+            
+        }
+
+        if (nearest.transform != null)
+        {
+            Transform hitCube = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+            hitCube.localScale = Vector3.one * 0.1f;
+            hitCube.position = nearest.point;
+        }
+    }
+}
